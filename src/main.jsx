d1a9b257e9dbcfe763a081428ef7dfdf276bdb57
JRodParreira <joseparreira99@gmail.com>
2026-04-20 11:34:21 +0100

    changes in main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { LanguageProvider } from './context/LanguageContext' // Importa o provider
 import { ThemeProvider } from './context/ThemeContext'
 import App from './App.jsx'
@@ -10,9 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <LanguageProvider> {/* O Provider vem primeiro */}
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </LanguageProvider>
     </ThemeProvider>
   </React.StrictMode>,
